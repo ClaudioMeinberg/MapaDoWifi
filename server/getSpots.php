70eaff54872435cf1a20c9105cc97689e03c2e78
Claudio Meinberg <claudio.meinberg@gmail.com>
2013-01-30 21:11:59 -0200

    Corrige encode do JSON

--- server/getSpots.php
+++ server/getSpots.php
@@ -7,15 +7,26 @@
 
 	if (!is_numeric($lat) || !is_numeric($long)) die('Error: Invalid data.');
 
-	header('Content-Type: application/javascript');
-
-	print('[');
+	//header('Content-Type: application/javascript');
+	header('Content-Type: application/json');
 
 	$strSQL = "SELECT lat, `long`, type, name, address, SSID, protected, password, effdt FROM mapa";
 	$query = mysql_query($strSQL);
 
+	$results = array();
+
 	while($rs = mysql_fetch_assoc($query)){
-		print json_encode($rs). ',' ;
-	}
-	print(']');
+	   	$results[] = array(
+	      'lat' => $rs['lat'],
+	      'long' => $rs['long'],
+	      'type' => $rs['type'],
+	      'name' => $rs['name'],
+	      'address' => $rs['address'],
+	      'SSID' => $rs['SSID'],
+	      'protected' => $rs['protected'],
+	      'password' => $rs['password'],
+	      'effdt' => $rs['effdt']
+	   	);
+	};
+	print json_encode($results);
 ?>
